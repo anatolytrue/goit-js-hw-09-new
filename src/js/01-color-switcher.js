ad53a86270435331873dfdf2c9c6028e103429c7
Anatolii <anatoly.tomnyak@gmail.com>
2023-08-08 12:38:59 +0300

    1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.body
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+let timerId = null
 
+startBtn.addEventListener('click', handleStart)
+stopBtn.addEventListener('click', handleStop)
+
+function handleStart() {
+    timerId = setInterval(changeColor, 1000);
+    startBtn.disabled = true
+}
+function handleStop() {
+    clearInterval(timerId)
+    startBtn.disabled = false
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+function changeColor() {
+    const newColor = getRandomHexColor()
+    body.style.backgroundColor = newColor
+}
